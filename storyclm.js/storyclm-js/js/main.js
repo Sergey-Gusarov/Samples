6bcdde606f73b5ab57a977d39d5a0f2abd5ef960
Vladimir Klyuev <rsk-k161@hotmail.com>
2017-04-20 16:38:19 +0300

    no message

--- storyclm.js/storyclm-js/js/main.js
+++ storyclm.js/storyclm-js/js/main.js
@@ -1,12 +1,13 @@
 var clickEvent = ('ontouchstart' in window ? 'touchend' : 'click');
 
 function b64_to_utf8(str) {
-    return decodeURIComponent(escape(window.atob(str)));
+    //return decodeURIComponent(escape(window.atob(str)));
+    return window.atob(str);
 }
 
 function setJson(id, json) {
     $("#" + id).html("<pre>" + JSON.stringify(json, null, 4) + "</pre>");
-    $("#body").html("<pre>" + b64_to_utf8(json.data.data.body) + "</pre>");
+    $("#responsebody").html("<pre>" + b64_to_utf8(json.data.body) + "</pre>");
 }
 
 function loadDocData(id)
